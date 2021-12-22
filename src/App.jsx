bcb36b78b98b5848297fadd140f93dcd0ad04c80
Toby Saunders <toby.saunders@live.co.uk>
2021-12-22 15:32:59 +0000

    fix page issues when firing a past search

--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,7 @@ function App() {
     pastSearchSpecies
   ) => {
     setDisplayedCharacters([])
+    setPageNum(1)
     setCharacterName(pastSearchCharacterName)
     setStatus(pastSearchStatus)
     setGender(pastSearchGender)
